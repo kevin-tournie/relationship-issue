dfb55d8cd4bdf8db7f65475338156abfd0a6d8cc
ktournie <kevin.tournie@ifolia.fr>
2024-01-07 13:04:07 +0100

    one more query alternative

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Product from "../../Models/Product";
+import Database from "@ioc:Adonis/Lucid/Database";
 
 export default class ProductsController {
   public async create({ request, response }: HttpContextContract) {
@@ -11,12 +12,25 @@ export default class ProductsController {
   public async find({ request, response }: HttpContextContract) {
     const { id } = request.params();
 
-    const product = await Product.findOrFail(id);
-    await product.load("category");
-    /**
-     * Alternative (mais retourne un tableau)
-     * const product = await Product.query().where(id, 'id').preload('category')
-     */
+    // const product = await Product.findOrFail(id);
+    // await product.load("category");
+
+    // Alternative 1
+
+    // const product = await Product.query().where("id", id).preload("category");
+
+    // Alternative 2 (surement la plus optimisée)
+
+    const product = await Database.query()
+      .select(
+        "p.id",
+        "p.name as product_name",
+        "p.state",
+        "c.name as category_name"
+      )
+      .from("products as p")
+      .where("p.id", id)
+      .leftJoin("categories as c", "c.id", "=", "p.category_id");
 
     return response.ok(product);
   }
